186701108f7f57f456338b4af80375270cc31c12
nahuelXZV <zvnahuel63@gmail.com>
2026-05-30 18:24:59 -0400

    add: agregado diseño para el punto de venta

---
+++ WebClient/Controllers/Sales/VentaController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebClient.Models;
+using WebClient.Models.Sales;
+using WebClient.Services;
+
+namespace WebClient.Controllers.Sales;
+
+public class VentaController : MainController
+{
+    private readonly ILogger<VentaController> _logger;
+
+    public VentaController(ViewModelFactory viewModelFactory, ILogger<VentaController> logger, IAppServices services)
+        : base(viewModelFactory, services)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> VentaDirecta()
+    {
+        var model = _viewModelFactory.Create<VentaViewModel>();
+        model.IncluirBlazorComponents = true;
+        return View(model);
+    }
+}
---
+++ WebClient/Models/Sales/VentaViewModel.cs
@@ -0,0 +1,12 @@
+﻿using Domain.DTOs.Inventory;
+using Domain.DTOs.Sales;
+using Domain.DTOs.Security;
+
+namespace WebClient.Models.Sales;
+
+public class VentaViewModel : MainViewModel
+{
+ 
+
+    public VentaViewModel() : base() { }
+}
